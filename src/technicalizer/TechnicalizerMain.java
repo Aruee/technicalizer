7c59f1666875d3bf821cc465d6aa83b837670f5b
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-09-12 14:53:11 +0200

    Revert "Only straight lines"
    This reverts commit 9cfcd9a2beaf2e03cbcf72758909b1c31cc9d563.

--- src/technicalizer/TechnicalizerMain.java
+++ src/technicalizer/TechnicalizerMain.java
@@ -75,10 +75,8 @@ public class TechnicalizerMain {
 					System.out.print(current.getX() + "," + current.getY() + " --> ");
 					for (int i = 0; i < points.size(); i++) {
 						Point toCheck = points.get(i);
-						if (	(Math.abs(current.getX() - toCheck.getX()) < 2 &&
-								Math.abs(current.getY() - toCheck.getY()) == 0) ||
-								(Math.abs(current.getY() - toCheck.getY()) < 2 &&
-								Math.abs(current.getX() - toCheck.getX()) == 0)) {
+						if (	Math.abs(current.getX() - toCheck.getX()) < 2 &&
+								Math.abs(current.getY() - toCheck.getY()) < 2) {
 							connection.add(toCheck);
 							current = toCheck;
 							points.remove(i);
